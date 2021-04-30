ea877a0e3cefc3669b85dfcba23bebf149351caa
Athena <athena.partch@gmail.com>
2021-04-30 15:52:04 -0700

    update products js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -19,7 +19,7 @@ export default class ProductList extends Component {
                         <ProductConsumer>
                             {(value)=>{
                                return value.products.map( product =>{
-                                   return <Product key={product.id} product={product}/>;
+                                   return <Product key={product.id} product={product} />;
                                })
                             }}
                         </ProductConsumer>
